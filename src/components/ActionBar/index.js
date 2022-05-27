617d8f7b46fe3c8f9622e8e82f9b1463259e45d3
Brian Cheng <quasifu@gmail.com>
2022-05-26 19:45:49 -0500

    add first version of action bar (#4)
    * add first version of action bar
    
    * updating pr workflow
    
    * push against left
    
    * adding more action bar styling
    
    Co-authored-by: quasifu <brian@cheng.ninja>

--- src/components/ActionBar/index.js
+++ src/components/ActionBar/index.js
@@ -1,23 +1,49 @@
 import appStyle from '../../App.module.scss';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import style from './ActionBar.module.scss';
+import Chevron from 'react-chevron';
+import AnimateHeight from 'react-animate-height';
 
 function ActionBar() {
   const navigate = useNavigate();
+  const [open, setOpen] = useState(false);
+
+  function navigateToPage(page) {
+    navigate(page);
+    setOpen(false);
+  }
+
   return (
-    <div className={style.actionBar}>
-      <button className={appStyle.toggleButton} onClick={() => navigate('/')}>
-        Home
-      </button>
-      <button
-        className={appStyle.toggleButton}
-        onClick={() => navigate('/about')}
-      >
-        Learn More
-      </button>
-      <button className={appStyle.toggleButton}>Share your story</button>
-      <button className={appStyle.toggleButton}>Buy this domain</button>
-    </div>
+    <>
+      <div className={style.actionBarWrapper}>
+        <div className={style.actionBarHandle} onClick={() => setOpen(!open)}>
+          <Chevron direction={open ? 'down' : 'up'} />
+        </div>
+        <AnimateHeight
+          id='actionBar'
+          duration={500}
+          height={open ? 'auto' : 0} // see props documentation below
+        >
+          <div className={open ? style.actionBar : style.closedActionBar}>
+            <button
+              className={appStyle.toggleButton}
+              onClick={() => navigateToPage('/')}
+            >
+              Home
+            </button>
+            <button
+              className={appStyle.toggleButton}
+              onClick={() => navigateToPage('/about')}
+            >
+              Learn More
+            </button>
+            <button className={appStyle.toggleButton}>Share your story</button>
+            <button className={appStyle.toggleButton}>Buy this domain</button>
+          </div>
+        </AnimateHeight>
+      </div>
+    </>
   );
 }
 
